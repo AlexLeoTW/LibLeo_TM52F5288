580925574ad3591ce60b6032f2bd38117d46bdc3
AlexLeo <alex@alexleo.cu.cc>
2015-05-24 18:19:59 +0800

    NOTICE: New file are not yet tested.

---
+++ dev/16digi-calc.h
@@ -0,0 +1,15 @@
+#ifndef 16DIGI_CALC_H
+#define 16DIGI_CALC_H
+
+#include "common.h"
+#define opAIn 1
+#define opIn 2
+#define opBIn 3
+
+#define ADDSIGN 43
+#define SUBSIGN 45
+#define MULSIGN 42
+#define DIVSIGN 47
+#define CLEARSIGN 63
+
+#endif
